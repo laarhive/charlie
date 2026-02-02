84ed8122f6f2fa820b17b6e71dd65e9163b42b10
LaArhive <laarhive@protonmail.com>
2026-02-02 17:04:55 +0200

    Add missing file header comment for `deviceError.js`

--- src/devices/deviceError.js
+++ src/devices/deviceError.js
@@ -1,3 +1,4 @@
+// src/devices/deviceError.js
 export const deviceError = function deviceError(code, message, detail) {
   const err = new Error(message || String(code || 'DEVICE_ERROR'))
   err.code = String(code || 'DEVICE_ERROR')
